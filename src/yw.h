d72772fbb1c5e8d4b4d7df715acc861a7dc7ad48
Erik Williamson <ewilliamson@gloo.us>
2026-09-02 09:24:33 -0700

    further tweaks to vehicle control when jumping in

--- src/yw.h
+++ src/yw.h
@@ -2805,7 +2805,7 @@ public:
     }
 
     void ypaworld_func64__sub1(TInputState *inpt);
-    void ypaworld_func64__sub21__sub5(int arg);
+    void ypaworld_func64__sub21__sub5(int arg, TInputState *inpt);
 
 
     bool ypaworld_func64__sub21__sub6(TClickBoxInf *winp);
@@ -3323,6 +3323,7 @@ public:
 
     bool _fireBtnIsDown = false; // true - fire btn is down, contigues
     bool _fireBtnDownHappen = false; // true happen on down, single
+    bool _suppressFireUntilRelease = false; // possession click must not fire the entered unit
     bool _weaponSwitchBtnIsDown = false; // edge-triggered manual weapon selection
     bool _cycleTargetBtnIsDown = false;   // edge-triggered homing target cycle
     bool _ufoSpyBindingIsDown = false;     // edge latch for the remappable UFO Spy Mode binding
